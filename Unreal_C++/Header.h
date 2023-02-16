0db464872d3accf0978144c023db7d482c85eb81
Nikita Egorov <Pronobro15@gmail.com>
2023-02-17 00:12:15 +0300

    Добавьте файлы проекта.

---
+++ Unreal_C++/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <math.h>
+
+
+int Qutro(int a, int b)
+{
+	int Qutro;
+
+	Qutro = (pow(a, 2)) + (2 * a * b) + (pow(b, 2));
+
+	return Qutro;
+
+}
---
+++ Unreal_C++/Unreal_C++.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+#include "Header.h" // Пр р 13.4
+
+
+using namespace std;
+
+int main()
+{
+    // Пр р 13.4
+
+    int x = 2, y = 3, z = 0;
+    z = Qutro(x, y);
+
+    cout << "Pr r 13.4\n";
+    cout << "x=2 y=3\n";
+    cout << "x^2+2xy+y^2=" << z << "\n";
+
+    //Пр р 14.4
+
+    string str = "Hello World!";
+    char str1;
+    cout << "Pr r 14.4\n";
+    cout << str << "\n";
+    cout << str.length() << "\n";
+    str1 = str[0];
+    cout << str1 << "\n";
+    str1 = str[str.length() - 1];
+    cout << str1 << "\n";
+
+
+}
+
