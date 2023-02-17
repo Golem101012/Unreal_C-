0958ee3a3b639b8ad18af17e229a8ae72ed86fd5
Nikita Egorov <Pronobro15@gmail.com>
2023-02-17 20:00:48 +0300

    PR 16

--- Unreal_C++/Unreal_C++.cpp
+++ Unreal_C++/Unreal_C++.cpp
@@ -1,32 +1,43 @@
 ﻿
 
 #include <iostream>
+#include <time.h>
+
 
 
 using namespace std;
 
 
-void CoutNumber(int N,int j )
-{
-	for (int i = j; i < N + 1; i+=2)
-	{
 
-		cout << i<<"\n";
-
-	}
-
-}
 
 
 int main()
 {
+	struct tm buf;
+	time_t t = time(NULL);
+	localtime_s(&buf, &t);
+	const int N = 5;
 	setlocale(LC_ALL, "Russian");
-	const int N = 10;
-	cout << "Вывод четных чисел\n";
-	CoutNumber(N,0);
-	cout << "Вывод нечетных чисел\n";
-	CoutNumber(N, 1);
 	
+	int arr[N][N];
+
+	for (int i = 0; i < N; i++)
+
+	{
+		for (int j = 0; j < N; j++)
+		{
+			arr[i][j] = i + j;
+			cout << arr[i][j] << "  ";
+		}
+		cout << "\n";
+
+	}
+
+	cout << "\n";
+
+	for (int j = 0; j < N; j++)
+	{
+		cout << arr[buf.tm_mday%5][j] << "  ";
+	}
 
 }
-
