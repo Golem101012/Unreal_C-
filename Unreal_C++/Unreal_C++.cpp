09db9bfe72f49a03807660388a3d148114c98eea
Nikita Egorov <Pronobro15@gmail.com>
2023-02-18 13:23:20 +0300

    PR 17

--- Unreal_C++/Unreal_C++.cpp
+++ Unreal_C++/Unreal_C++.cpp
@@ -1,48 +1,77 @@
 ﻿
 
 #include <iostream>
-#include <time.h>
-
-
+#include "Math.h"
 
 using namespace std;
 
 
 
+class Vec {
+private:
 
+    double x;
+    double y;
+    double z;
+
+public:
+
+    Vec() : x(0), y(0), z(0)
+    {
+
+    }
+
+    Vec(double newX, double newY, double newZ) : x(newX), y(newY), z(newZ)
+    {
+
+    }
+
+    double GetX()
+    {
+        return x;
+    }
+
+    double GetY()
+    {
+        return y;
+    }
+
+    double GetZ()
+    {
+        return z;
+    }
+    void SetX(double newX)
+    {
+        x = newX;
+    }
+
+    void SetY(double newY)
+    {
+        y = newY;
+    }
+    void SetZ(double newZ)
+    {
+        z = newZ;
+    }
+
+
+    void ShowVector()
+    {
+        cout << "\n" << x << " " << y << " " << z;
+    }
+
+    double  LenghtVector()
+    {
+        return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    }
+
+};
 
 int main()
 {
-	struct tm buf;
-	time_t t = time(NULL);
-	localtime_s(&buf, &t);
-	const int N = 5;
-	int sum_str = 0;
-	setlocale(LC_ALL, "Russian");
-	
-	int arr[N][N];
-
-	for (int i = 0; i < N; i++)
-
-	{
-		for (int j = 0; j < N; j++)
-		{
-			arr[i][j] = i + j;
-			cout << arr[i][j] << "  ";
-		}
-		cout << "\n";
-
-	}
-
-	cout << "\n";
-
-	for (int j = 0; j < N; j++)
-	{
-		cout << arr[buf.tm_mday%N][j] << "  ";
-		sum_str += arr[buf.tm_mday % N][j];
-	}
-	cout <<"\n" << sum_str << "\n";
-
+    Vec temp(2, 3, 4);
+    temp.ShowVector();
+    cout << "\n" << temp.LenghtVector();
 
 
 }
