d698ee91478022599ad776570df1a16a56ead3c0
Nikita Egorov <Pronobro15@gmail.com>
2023-02-19 11:17:21 +0300

    PR 19

--- Unreal_C++/Unreal_C++.cpp
+++ Unreal_C++/Unreal_C++.cpp
@@ -1,116 +1,77 @@
 ﻿
-
 #include <iostream>
 #include "Math.h"
 
 using namespace std;
 
 
-class MyPlayer{
-private:	
-	string PlayerName;
 
-	int Score;
+class Animal {
+private:
 
 public:
-	MyPlayer() : PlayerName("NULL"), Score(0)
-	{
 
+	Animal() {}
+
+	virtual void Voice()
+	{
+		cout << "rrr\n";
 	}
 
-	MyPlayer(string newName, int newScore) : PlayerName(newName), Score(newScore)
-	{
 
-	}
-
-	void SetName(string newName)
-	{
-		PlayerName = newName;
-	}
-
-	void SetScore(int newScore)
-	{
-		Score = newScore;
-	}
-
-	string GetName() 
-	{
-		return PlayerName;
-	}
-
-	int GetScore() 
-	{
-		return Score;
-	}
-
-	void Show()
-	{
-		cout << "Имя " << PlayerName << " Кол-во очков " << Score;
-	}
 
 };
 
 
-void Show(MyPlayer *arr, int x)
+class Dog : public Animal
 {
-		for (int i = 0; i < x; i++)
-		{
+private:
 
-		  arr[i].Show();
-		  cout << "\n";
+public:
+	Dog() {}
 
-	    }
-}
+	void Voice() override
+	{
+		cout << "Woof!\n";
+	}
+};
 
+class Cat : public Animal
+{
+private:
 
+public:
+	Cat() {}
 
+	void Voice() override
+	{
+		cout << "Mya!\n";
+	}
+};
+class Pig : public Animal
+{
+private:
 
+public:
+	Pig() {}
+
+	void Voice() override
+	{
+		cout << "Hr Hr!\n";
+	}
+};
 
 int main()
 {
-	setlocale(LC_ALL, "ru");
-	int x,nSCORE;
-	string sNAME;
 
-	cout << "Кол-во игроков\n";
-	cin >> x;
-	MyPlayer *arr = new MyPlayer[x];
+	Animal* arr[3];
 
-	for (int i = 0; i < x; i++)
-	{
-		cout << "Имя игрока\n";
-		cin >> sNAME;
-		arr[i].SetName(sNAME);
-		cout << "Кол-во очков\n";
-		cin >> nSCORE;
-		arr[i].SetScore(nSCORE);
-	}
+	arr[0] = new Dog();
+	arr[1] = new Cat();
+	arr[2] = new Pig();
 
-	cout << "\n";
-
-	Show(arr,x);
-
-	
-	for (int i = 0; i < x; i++) 
-	{
-
-
-
-		for (int j = 0; j < x - 1; j++)
-		{
-			if (arr[j].GetScore() < arr[j + 1].GetScore())
-			{
-				swap(arr[j], arr[j+1]);
-			}
-
-		}
-	}
-
-	Show(arr, x);
-	
-	delete[] arr;
-
-
+	for (Animal* a : arr)
+		a->Voice();
 }
 
 
